83bb4a4dd9da288c40914c94360d2ab30ef1afd1
wei <nuquejmrvn@gmail.com>
2024-10-29 19:59:18 +0800

    username

--- app/api/serializers/user_serializers.py
+++ app/api/serializers/user_serializers.py
@@ -242,12 +242,11 @@ class ChangePasswordSerializer(serializers.Serializer):
         return attrs
     
 class UsersSerializer(serializers.ModelSerializer):
-    full_name = serializers.SerializerMethodField()
-
+    # full_name = serializers.SerializerMethodField()
     class Meta:
         model = User
         # fields = ['full_name', 'contact_number', 'is_email_verified', 'role', 'is_active', 'is_verified']
-        fields = ['full_name', 'contact_number', 'is_verified', 'violation', 'role', 'account_status', 'address', 'email']
+        fields = ['username', 'contact_number', 'is_verified', 'violation', 'role', 'account_status', 'address', 'email']
 
-    def get_full_name(self, obj):
-        return f"{obj.first_name} {obj.last_name}"
+    # def get_full_name(self, obj):
+    #     return f"{obj.first_name} {obj.last_name}"
