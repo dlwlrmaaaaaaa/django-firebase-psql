6c9b0f26e978b0a29d3b0538e5365981fb4459ab
Justin Marucut <143764790+dlwlrmaaaaaaa@users.noreply.github.com>
2024-12-08 19:07:44 +0800

    commit

--- app/app/firebase.py
+++ app/app/firebase.py
@@ -1,10 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, storage, firestore
-
+import os
 # Initialize Firebase
 # cred = credentials.Certificate('C:/Users/ADMIN/Documents/crisp-5d09f-firebase-adminsdk-vl4cg-30e5cb1ca3.json')
 # cred = credentials.Certificate('C:/Users/Siyan/Desktop/Projects/Thesis-BSCS/crisp-63736-firebase-adminsdk-r1i8j-a043e2e3ad.json')
-cred = credentials.Certificate('C:/Users/codin/OneDrive/crisp-63736-firebase-adminsdk-r1i8j-a043e2e3ad.json')
+cred = credentials.Certificate(os.environ.get("FIREBASE_CRED"))
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
