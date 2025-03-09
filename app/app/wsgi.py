be8cd1b2a48779d157c90f7e6be0067ca0087b3d
Justin Marucut <143764790+dlwlrmaaaaaaa@users.noreply.github.com>
2025-03-09 16:32:59 +0800

    commit

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, BASE_DIR)
 
